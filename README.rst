a6f25bd41fb9481d6cf9028e6ef3c4cedf08dcbd
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-06-16 00:23:46 -0400

    A working version for IPython 3.x and 4.0dev.

---
+++ README.rst
@@ -0,0 +1,40 @@
+Gamepad and joystick support for the Jupyter notebook
+=====================================================
+
+License
+-------
+
+BSD License. Copyright 2015 - Sylvain Corlay
+
+Installation
+------------
+
+- Install the Python package
+
+.. code-block:: python
+
+    pip install gamepad
+
+of for a development install:
+
+.. code-block:: python
+
+    git clone https://github.com/SylvainCorlay/gamepad.git
+    pip install -e gamepad
+
+- Install and register the notebook extension
+
+.. code-block:: python
+
+    python -m gamepad.install
+
+or for a development install:
+
+.. code-block:: python
+
+    python -m gamepad.install --user --symlink
+
+Usage
+-----
+
+See example notebook.
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+    from setuptools.command.install import install
+except ImportError:
+    from distutils.core import setup
+    from distutils.core.command.install import install
+
+
+setup(
+    name='gamepad',
+    version='0.1',
+    description='Gamepad and Joystick support for the Jupyter notebook.',
+    author='Sylvain Corlay',
+    author_email='sylvain.corlay@gmail.com',
+    license='New BSD License',
+    url='https://github.com/SylvainCorlay/gamepad',
+    keywords='gamepad joystick simulator flight game python ipython widgets widget',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Programming Language :: Python'],
+    packages=['gamepad'],
+)
