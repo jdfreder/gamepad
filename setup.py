f923aaad89b6497413c348fdaafc2088d70ce3a5
Sylvain Corlay <scorlay@bloomberg.net>
2015-06-18 19:40:21 +0000

    distribute nbextension on pypi

--- setup.py
+++ setup.py
@@ -2,22 +2,21 @@
 
 try:
     from setuptools import setup
-    from setuptools.command.install import install
 except ImportError:
     from distutils.core import setup
-    from distutils.core.command.install import install
 
 
 setup(
     name='gamepad',
-    version='0.1.1',
+    version='0.1.2',
     description='Gamepad and Joystick support for the Jupyter notebook.',
     author='Sylvain Corlay',
     author_email='sylvain.corlay@gmail.com',
     license='New BSD License',
     url='https://github.com/SylvainCorlay/gamepad',
+    packages=['gamepad'],
+    include_package_data=True,
     keywords='gamepad joystick simulator flight game python ipython widgets widget',
     classifiers=['Development Status :: 4 - Beta',
                  'Programming Language :: Python'],
-    packages=['gamepad'],
 )
