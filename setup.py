539a1588581f97a93640a1b359fa90aaec991cf1
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-06-18 14:31:26 -0400

    Bump version 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='gamepad',
-    version='0.1',
+    version='0.1.1',
     description='Gamepad and Joystick support for the Jupyter notebook.',
     author='Sylvain Corlay',
     author_email='sylvain.corlay@gmail.com',
